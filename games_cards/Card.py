f8b0e18085d7c42114f79f5c89d0252d2246b37c
avish <avishai,g97@gmial.com>
2020-08-04 23:14:20 +0300

    card_1 Create a playing card

---
+++ games_cards/Card.py
@@ -0,0 +1,16 @@
+
+
+class Card:
+
+    def __init__(self, value, suit):
+        # Create a playing card
+        self.value = value
+        if self.value <= 0 or self.value > 13:
+            self.value = 0
+            raise ValueError("value should be between 1 and 13")
+        self.suit = suit
+        if self.suit not in ["Diamond", "Spade", "Heart", "Club"]:
+            raise ValueError("value should be one of: Diamond Spade Heart Club")
+
+
+
