50d414de00ed733be9928897303e4475f232b0b2
avish <avishai,g97@gmial.com>
2020-08-04 23:26:44 +0300

    DeckOfCards func

--- games_cards/DeckOfCards.py
+++ games_cards/DeckOfCards.py
@@ -17,3 +17,24 @@ class DeckOfCards:
         # Shuffling a deck of cards
         shuffle(self.pack)
 
+    def deal_one(self):
+        # deal card from deck of cards
+        return self.pack.pop(0)
+
+    def new_game(self):
+        # Create a deck of cards and shuffle the deck
+        self.pack = []
+        suits = ["Diamond", "Spade", "Heart", "Club"]
+        for suit in suits:
+            for num in range(1, 14):
+                c = Card(num, suit)
+                self.pack.append(c)
+        shuffle(self.pack)
+
+    def show(self):
+        # Print a deck of card
+        return f"pack- {self.pack}"
+
+    def __repr__(self):
+        # Print a deck of card
+        return self.show()
